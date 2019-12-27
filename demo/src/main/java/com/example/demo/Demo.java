90532c782e6530fa6910cebb1cb5441391d0bd6f
sk143 <sk1433223@qq.com>
2019-12-27 23:50:23 +0800

    2019/12/27 22:58

--- demo/src/main/java/com/example/demo/Demo.java
+++ demo/src/main/java/com/example/demo/Demo.java
@@ -1,4 +1,10 @@
 package com.example.demo;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("我写了一句话");
+    }
 }
--- demo/src/main/java/com/example/demo/sorting/BubbleSort.java
+++ demo/src/main/java/com/example/demo/sorting/BubbleSort.java
@@ -19,15 +19,18 @@ public class BubbleSort {
      * @return
      */
     public static int[] bubbleSort(int[] array) {
-        if (array.length == 0)
+        if (array.length == 0) {
             return array;
-        for (int i = 0; i < array.length; i++)
-            for (int j = 0; j < array.length - 1 - i; j++)
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j + 1] < array[j]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
                 }
+            }
+        }
         return array;
     }
 
