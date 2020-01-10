e433f7a2996b2b63045500501c321774667e49f0
sk143 <sk1433223@qq.com>
2020-01-10 21:54:37 +0800

    Java排序算法

--- demo/src/main/java/com/example/demo/sorting/SelectionSort.java
+++ demo/src/main/java/com/example/demo/sorting/SelectionSort.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Array;
  * @Description: 选择排序
  *      原理：首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中
  *      继续寻找最小（大）元素，然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
- * @author AK
+ * @author 阿康
  * @ClassName: SelectionSort
  * @date 2019/12/30 16:33
  */
